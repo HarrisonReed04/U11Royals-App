948e66338165bdbdb1bf293cb8aad613cf1edebe
Harrison Reed <harrisonreed.tech>
2022-08-05 16:15:16 +0100

    Initial Commit

---
+++ U11Royals/U11RoyalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  U11RoyalsApp.swift
+//  U11Royals
+//
+//  Created by Harrison Reed on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct U11RoyalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
