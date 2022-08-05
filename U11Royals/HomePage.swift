39a2fe0ca7d00ef29729fa678eac22172924bd89
Harrison Reed <harrisonreed.tech>
2022-08-05 16:53:22 +0100

    Initial files made

---
+++ U11Royals/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  U11Royals
+//
+//  Created by Harrison Reed on 05/08/2022.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
---
+++ U11Royals/MatchesView.swift
@@ -0,0 +1,20 @@
+//
+//  MatchesView.swift
+//  U11Royals
+//
+//  Created by Harrison Reed on 05/08/2022.
+//
+
+import SwiftUI
+
+struct MatchesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MatchesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatchesView()
+    }
+}
